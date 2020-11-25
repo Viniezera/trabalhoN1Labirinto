725336ecae4d71984025ba64b8d6f7b14f82fd47
Viniezera <freire.vinie@gmail.com>
2020-11-25 16:51:50 -0300

    Update Labirinto.cpp

--- Labirinto.cpp
+++ Labirinto.cpp
@@ -2,32 +2,5 @@
 //
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include "structrato.h"
-using namespace std;
-int main()
-{
-	bool andar(char labirinto[5][5]), int x, int y, char deOndeVeio, bool& queijo) {
-	labirinto[x][y] = 'r';
-	if (queijo == true) {
-		return true;
-	}
-	else if (labirinto[x][y] == 'Q') {
-		return true;
-	}
-	else if (labirinto[x][y] == 'e') {
-		return false;
-	}
-	else if (labirinto[x][y] == 'p') {
-		return false;
-	}
-	else if (labirinto[x > 5 || x < 5]) {
-		return false;
-	}
-	else if (labirinto[y > 5 || y < 5]) {
-		return false;
-	}
-	}
-}
-
+#include "ratao.h"
+#include "mapinha.h"
